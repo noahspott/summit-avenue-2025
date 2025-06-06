649cb884bb23287280a1e7c54a2cd3b0b5f90cba
noahspott <noahspottdev@gmail.com>
2025-06-06 08:48:07 -0300

    feat: basic Navbar component

---
+++ src/assets/data/links.ts
@@ -0,0 +1,27 @@
+interface Link {
+  display: string;
+  href: string;
+}
+
+export const LINKS: Link[] = [
+  {
+    display: "Work",
+    href: "/Work",
+  },
+  {
+    display: "Services",
+    href: "/Services",
+  },
+  {
+    display: "About",
+    href: "/About",
+  },
+  {
+    display: "Blog",
+    href: "/Blog",
+  },
+  {
+    display: "Contact",
+    href: "/Contact",
+  },
+];
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,2 @@
 export { default as Navbar } from "./Navbar.astro";
-export { default as Content } from "./Content.astro";
+export { default as Content } from "./Content";
