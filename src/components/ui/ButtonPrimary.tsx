194ad1c2703c13d57bf07cb17842e723aa13690e
noahspott <noahspottdev@gmail.com>
2025-06-10 10:57:38 -0300

    feat: primary button hover styling

--- src/components/ui/ButtonPrimary.tsx
+++ src/components/ui/ButtonPrimary.tsx
@@ -6,12 +6,29 @@ export default function ButtonPrimary({
   className?: string;
 }) {
   return (
-    <div className={`gradient-blue-bg rounded-4xl p-[2px] ${className}`}>
-      <div className="bg-white rounded-4xl">
-        <button className="text-xl font-montserrat font-2 self-start bg-white py-4 px-8 rounded-4xl gradient-blue-text">
+    <button
+      className={`group rounded-4xl p-[2px] button-primary__border hover:cursor-pointer hover:shadow-md hover:scale-[101%] ${className}`}
+    >
+      <div className="bg-white rounded-4xl flex gap-2.5 items-center py-4 px-8">
+        <div className="group-hover:cursor-pointer text-xl font-montserrat font-2 rounded-4xl button-primary__text">
           {children}
-        </button>
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#1d3557"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-move-right-icon lucide-move-right button-primary__arrow"
+        >
+          <path d="M18 8L22 12L18 16" />
+          <path d="M2 12H22" />
+        </svg>
       </div>
-    </div>
+    </button>
   );
 }
