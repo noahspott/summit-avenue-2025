d4119e313e1db723f399d8f4cdd2e5b9c77654e8
noahspott <noahspottdev@gmail.com>
2025-06-11 11:21:43 -0300

    feat: ButtonSecondary

---
+++ src/components/ui/ButtonSecondary.tsx
@@ -0,0 +1,37 @@
+import type React from "react";
+
+export default function ButtonSecondary({
+  children,
+  href,
+  className,
+}: {
+  children: React.ReactNode;
+  href: string;
+  className?: string;
+}) {
+  return (
+    <a
+      className={`button-secondary relative flex flex-row gap-2.5 items-center text-white ${className}`}
+      href={href}
+    >
+      <div className="text-xl link-underline font-montserrat font-2 pt-4 mb-4">
+        {children}
+      </div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="#ffffff"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="button__arrow"
+      >
+        <path d="M18 8L22 12L18 16" />
+        <path d="M2 12H22" />
+      </svg>
+    </a>
+  );
+}
