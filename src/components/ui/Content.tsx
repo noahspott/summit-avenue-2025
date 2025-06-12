19f637963780eba785048992683c708ddcca4985
noahspott <noahspottdev@gmail.com>
2025-06-12 14:02:18 -0300

    feat: BackgroundLogo

--- src/components/ui/Content.tsx
+++ src/components/ui/Content.tsx
@@ -1,6 +1,6 @@
 export default function Content({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative px-4 md:px-8 lg:px-16 max-w-[1344px] mx-auto">
+    <div className="relative px-4 lg:px-0 max-w-[1344px] mx-auto">
       {children}
     </div>
   );
