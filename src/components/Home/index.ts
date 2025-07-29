7704e25ec9dfe462ca513a261844a1e59da45440
noahspott <noahspottdev@gmail.com>
2025-07-28 22:00:36 -0300

    feat: pricing section

--- src/components/Home/index.ts
+++ src/components/Home/index.ts
@@ -2,3 +2,4 @@ export { default as HeroSection } from "./HeroSection.astro";
 export { default as ServicesSection } from "./ServicesSection.astro";
 export { default as TestimonialsSection } from "./TestimonialsSection.astro";
 export { default as OurWorkSection } from "./OurWorkSection.astro";
+export { default as PricingSection } from "./PricingSection.astro";
